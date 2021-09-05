8ea3382a35f263de73ec80ddd304c209e770b33c
aTxr <alexandre.tullot@gmail.com>
2021-09-06 00:50:53 +0200

    Add check function to calculate coord of collision pt

--- include/Ray.h
+++ include/Ray.h
@@ -2,11 +2,13 @@
 #define RAY_H
 
 #include <SFML/Graphics.hpp>
+#include "Wall.h"
 
 class Ray {
     public:
         Ray();
         Ray(int theta);
+        sf::Vector2f check(sf::Vector2f origin, Wall *wall);
         void draw(sf::Vector2f origin, sf::RenderWindow *window);
 
     private:
--- src/Ray.cpp
+++ src/Ray.cpp
@@ -1,8 +1,41 @@
 #include "Ray.h"
 #include <math.h>
 
+float size(sf::Vector2f v)
+{
+    return sqrt(pow(v.x, 2) + pow(v.y, 2));
+}
+
 Ray::Ray() : m_theta(0) {}
 Ray::Ray(int theta) : m_theta(theta) {}
+
+sf::Vector2f Ray::check(sf::Vector2f origin, Wall *p_wall)
+{
+    float x1 = origin.x;
+    float y1 = origin.y;
+    float x2 = x1 + 100*cos(m_theta);
+    float y2 = y1 + 100*sin(m_theta);
+
+    float x3 = p_wall->get1().x;
+    float y3 = p_wall->get1().y;
+    float x4 = p_wall->get2().x;
+    float y4 = p_wall->get2().y;
+
+    float den = (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4);
+    if (den == 0)
+    {
+        return sf::Vector2f(0,0);
+    }
+
+    float t = ((x1 - x3)*(y3 - y4) - (y1 - y3)*(x3 - x4))/den;
+    float u = ((x2 - x1)*(y1 - y3) - (y2 - y1)*(x2 - x3))/den;
+    if (t>0 && u>0 && u<1)
+    {
+        return sf::Vector2f(x1 + t*(x2-x1), y1 + t*(y2-y1));
+    }
+
+    return sf::Vector2f(0,0);
+}
 
 void Ray::draw(sf::Vector2f origin, sf::RenderWindow *p_window)
 {
