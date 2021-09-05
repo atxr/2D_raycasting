6b312c0c64ff3553c8dec20a3a35295177faa84b
aTxr <alexandre.tullot@gmail.com>
2021-09-05 22:56:06 +0200

    Walls first try

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <SFML/Graphics.hpp>
+#include <time.h>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(800, 400), "sfml-app");
+    sf::RectangleShape lines[5];
+    
+    srand (time(NULL));
+    for (int i=0; i<5; i++)
+    {
+        lines[i] = sf::RectangleShape(sf::Vector2f(200 + i*50, 1));
+        lines[i].rotate(i*360/5);
+        lines[i].setPosition(rand() % 800, rand() % 400);
+    }
 
     while (window.isOpen())
     {
@@ -16,7 +24,11 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        for (int i=0; i<5; i++)
+        {
+            window.draw(lines[i]);
+        }
+
         window.display();
     }
 
